233797941e804856cf0990118b93d394e5d40f0c
gusevalex07 <agusev@erpone.ru>
2020-05-02 22:43:28 +0300

    Bot for InfoErp tickets

---
+++ Main.py
@@ -0,0 +1,39 @@
+import requests
+import telebot
+from telebot import types
+
+response = requests.get('http://usus:123@185.75.181.51/InfoErp/hs/GetTickets/')
+total= response.text
+
+bot = telebot.TeleBot('1271443472:AAEWba4JvbFKXVfJ5VOxuoZY2OiDrZF_4SM')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton('Сколько тикетов за год?')
+    btn2 = types.KeyboardButton('Халюков')
+    btn3 = types.KeyboardButton('Данильченко')
+    btn4 = types.KeyboardButton('Аскапов')
+    markup.add(btn1, btn2, btn3, btn4)
+
+    send_message = f"<b>Привет {message.from_user.first_name}!</b>\nЧтобы узнать данные по решенным тикетам напишите " \
+                   f"фамилию сотрудника, например: Данильченко и так далее\n\nЗаходи к нам на сайт <a href='https://new.erpone.ru'>ERP ONE</a>"
+    bot.send_message(message.chat.id, send_message, parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    final_message = ""
+    get_message_bot = message.text.strip().lower()
+    if get_message_bot == "данильченко":
+        info = 1555
+    elif get_message_bot == "халюков":
+        info = 2311
+    elif get_message_bot == "аскапов":
+        info = 233
+    else:
+        info = total
+    final_message = f"<u>Данные по тикетам:</u>\n<b>Решено: </b>{info:,}"
+    bot.send_message(message.chat.id, final_message, parse_mode='html')
+
