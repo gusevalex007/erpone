fd829ed7e3ae9bd575d9a038097cfd48df40875d
gusevalex07 <agusev@erpone.ru>
2020-05-04 16:01:43 +0300

    Bot for InfoErp tickets

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import requests
 import telebot
 from telebot import types
 
-response = requests.get('http://185.75.181.51/InfoErp/hs/GetTickets/', auth = requests.auth.HTTPBasicAuth('usus', '123'))
-total= str(response.text)
+#response = requests.get('http://185.75.181.51/InfoErp/hs/GetTickets/', auth = requests.auth.HTTPBasicAuth('usus', '123'))
+total= 5555#str(response.text)
 
 bot = telebot.TeleBot('1271443472:AAEWba4JvbFKXVfJ5VOxuoZY2OiDrZF_4SM')
 
